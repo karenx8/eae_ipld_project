24c6ba60d4d5e5ea9c918c7816ea424e630c2f7a
Karen Xerinda <karenxerinda88@gmail.com>
2025-02-18 16:32:06 +0100

    Netflix update

--- pages/2_🎬_Netflix_Data_Analysis.py
+++ pages/2_🎬_Netflix_Data_Analysis.py
@@ -54,7 +54,10 @@ max_year = movies_df["release_year"].max()
 num_missing_directors = movies_df["director"].isnull().sum()
 
 # TODO: Ex 2.4: How many different countries are there in the data?
-unique_countries = movies_df["country"].unique()
+movies_df["country"] = movies_df["country"].fillna("Unknown")
+total_countries = ", ".join(movies_df["country"]).split(", ")
+unique_countries = set(total_countries)
+unique_countries=[x for x in unique_countries if x.strip()]
 n_countries = len(unique_countries)
 
 # TODO: Ex 2.5: How many characters long are on average the title names?
--- pages/3_🌦️_Temperatures_Dashboard.py
+++ pages/3_🌦️_Temperatures_Dashboard.py
@@ -8,7 +8,7 @@ import streamlit as st
 
 # ----- Page configs -----
 st.set_page_config(
-    page_title="<Your Name> Portfolio",
+    page_title="Karen Xerinda Portfolio",
     page_icon="📊",
 )
 
