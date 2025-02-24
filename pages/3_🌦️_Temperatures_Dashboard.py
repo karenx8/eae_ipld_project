d3d568b2b73b98fbc3aa0f3950888a76bbc6c7c2
Karen Xerinda <karenxerinda88@gmail.com>
2025-02-24 18:56:03 +0100

    Temperature Fix Error 3

--- pages/3_🌦️_Temperatures_Dashboard.py
+++ pages/3_🌦️_Temperatures_Dashboard.py
@@ -112,7 +112,7 @@ st.header("Comparing the Temperatures of the Cities")
 
 if unique_countries_list is not None:
     # Getting the list of cities to compare from the user
-    selected_cities = st.multiselect("Select the cities to compare:", unique_countries_list, default=["Buenos Aires", "Dakar"], max_selections=4)
+    selected_cities = st.multiselect("Select the cities to compare:", unique_countries_list, default=["Argentina", "Senegal"], max_selections=4)
 
     cols2 = st.columns([6, 1, 6])
 
